88838947a8485819cd941fadcaa1a398de40c975
仇思宇 <siyu.qiu@smartmore.com>
2022-11-12 16:19:20 +0800

    feat: change some config

--- cmake/custom-triplets/x64-windows-mixed.cmake
+++ cmake/custom-triplets/x64-windows-mixed.cmake
@@ -1,5 +1,5 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
-if(${PORT} MATCHES "qtbase")
+if(${PORT} MATCHES "qt")
 	set(VCPKG_CRT_LINKAGE dynamic)
 	set(VCPKG_LIBRARY_LINKAGE dynamic)
 else()
--- cmake/fetch/FramelessHelper.cmake
+++ cmake/fetch/FramelessHelper.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
   framelesshelper
   GIT_REPOSITORY https://github.com/wangwenx190/framelesshelper.git
-  GIT_TAG 2.3.1
+  GIT_TAG 2.3.3
   GIT_SHALLOW ON
 )
 
